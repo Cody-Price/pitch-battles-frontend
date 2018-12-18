b015edea17b1dfde37713af36668dab68617a113
Kevin Simpson <relasine@gmail.com>
2018-12-18 16:05:18 -0700

    Add strings to instruments object

--- src/utilities/instruments.js
+++ src/utilities/instruments.js
@@ -530,5 +530,614 @@ const instruments = [
         level: 4
       }
     ]
+  },
+  {
+    instrument: "trumpet",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "below-space-two",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 3
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "below-line-two",
+        level: 4
+      },
+      {
+        pitch: "g",
+        position: "below-space-three",
+        level: 4
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "horn",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 2
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 2
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 3
+      },
+      {
+        pitch: "b",
+        position: "below-space-two",
+        level: 4
+      },
+      {
+        pitch: "a",
+        position: "below-line-two",
+        level: 4
+      },
+      {
+        pitch: "g",
+        position: "below-space-three",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "trombone",
+    clef: "bass",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-one",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "space-four",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "line-five",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-one",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-one",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "below-space-one",
+        level: 3
+      },
+      {
+        pitch: "c",
+        position: "above-line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "above-space-two",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "aboce-line-two",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "baritone",
+    clef: "bass",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-one",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "space-four",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "line-five",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-one",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-one",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "below-space-one",
+        level: 3
+      },
+      {
+        pitch: "c",
+        position: "above-line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "above-space-two",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "above-line-two",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: 'violin',
+    clef: 'treble',
+    pitches: [
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 2
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 3
+      },
+      {
+        pitch: "b",
+        position: "below-space-two",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "below-line-two",
+        level: 3
+      }
+      {
+        pitch: "g",
+        position: "below-space-three",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 4
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 4
+      }
+    ],
+    
+  },
+  {
+    instrument: 'viola',
+    clef: 'alto',
+    pitches: [
+      {
+        pitch: "d",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "above-space-one",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "above-space-two",
+        level: 2
+      },
+      {
+        pitch: "d",
+        position: "above-line-two",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "line-three",
+        level: 3
+      },
+      {
+        pitch: "b",
+        position: "space-two",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "space-one",
+        level: 4
+      },
+      {
+        pitch: "f",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "below-space-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: 'cello',
+    clef: 'bass',
+    pitches: [
+      {
+        pitch: "d",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "above-space-one",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "d",
+        position: "above-space-two",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "space-two",
+        level: 3
+      },
+      {
+        pitch: "b",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "space-one",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "f",
+        position: "below-space-one",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: 'string bass',
+    clef: 'bass',
+    pitches: [
+      {
+        pitch: "d",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "above-space-one",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "d",
+        position: "above-space-two",
+        level: 2
+      },
+      {
+        pitch: "c",
+        position: "space-two",
+        level: 3
+      },
+      {
+        pitch: "b",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "space-one",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "f",
+        position: "below-space-one",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
   }
 ];
