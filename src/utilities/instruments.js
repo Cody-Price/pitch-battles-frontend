832af45b0ab8285f7998e3f5d76d1833e6e36118
Kevin Simpson <relasine@gmail.com>
2018-12-18 15:06:01 -0700

    Add woodwinds

--- src/utilities/instruments.js
+++ src/utilities/instruments.js
@@ -1,1 +1,534 @@
-const instruments = {};
+const instruments = [
+  {
+    instrument: "flute",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "oboe",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "clarinet",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "below-space-one",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "below-line-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "below-space-three",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "below-line-three",
+        level: 3
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "alto saxophone",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 2
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 2
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 3
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "above-space-two",
+        level: 4
+      },
+      {
+        pitch: "b",
+        position: "above-line-two",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "tenor saxophone",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-two",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "c",
+        position: "above-line-two",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "baritone saxophone",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 2
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 2
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 3
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "above-space-two",
+        level: 4
+      },
+      {
+        pitch: "b",
+        position: "above-line-two",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "bassoon",
+    clef: "bass",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-two",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-two",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-one",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "space-four",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "line-five",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-one",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-one",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "below-space-one",
+        level: 3
+      },
+      {
+        pitch: "c",
+        position: "above-line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "above-space-two",
+        level: 4
+      },
+      {
+        pitch: "e",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  }
+];
