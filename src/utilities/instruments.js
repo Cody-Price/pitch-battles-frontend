c5d48110f1f92b3bd1592e2cbc6112fb3dd9d11f
Kevin Simpson <relasine@gmail.com>
2018-12-18 16:07:53 -0700

    Add percussion to instruments object

--- src/utilities/instruments.js
+++ src/utilities/instruments.js
@@ -1139,5 +1139,157 @@ const instruments = [
         level: 4
       }
     ]
-  }
+  },
+  {
+    instrument: "mallet percussion (high)",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "space-three",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "line-four",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "space-four",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "line-five",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "above-space-one",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "above-line-one",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "above-space-two",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 3
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 4
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 4
+      }
+    ]
+  },
+  {
+    instrument: "mallet percussion (low)",
+    clef: "treble",
+    pitches: [
+      {
+        pitch: "b",
+        position: "below-space-two",
+        level: 1
+      },
+      {
+        pitch: "c",
+        position: "below-line-one",
+        level: 1
+      },
+      {
+        pitch: "d",
+        position: "below-space-one",
+        level: 1
+      },
+      {
+        pitch: "e",
+        position: "line-one",
+        level: 1
+      },
+      {
+        pitch: "f",
+        position: "space-one",
+        level: 1
+      },
+      {
+        pitch: "a",
+        position: "below-line-two",
+        level: 2
+      },
+      {
+        pitch: "g",
+        position: "line-two",
+        level: 2
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 2
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 3
+      },
+      {
+        pitch: "g",
+        position: "below-space-three",
+        level: 3
+      },
+      {
+        pitch: "f",
+        position: "below-line-three",
+        level: 3
+      },
+      {
+        pitch: "a",
+        position: "space-two",
+        level: 4
+      },
+      {
+        pitch: "b",
+        position: "line-three",
+        level: 4
+      },
+      {
+        pitch: "c",
+        position: "space-four",
+        level: 4
+      }
+    ]
+  },
 ];
