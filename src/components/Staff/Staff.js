522459ea736e1771163ca1915a7efeb27ebb36b9
Kevin Simpson <relasine@gmail.com>
2018-12-18 12:27:17 -0700

    Create, populate, and place Staff component

---
+++ src/components/Staff/Staff.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./Staff.css";
+
+class Staff extends Component {
+  render() {
+    return (
+      <section className="staff">
+        <div className="clef" />
+        <div className="pitch" />
+      </section>
+    );
+  }
+}
+
+export default Staff;
