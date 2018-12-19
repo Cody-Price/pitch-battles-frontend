47e105ea25e4e70ab848316fa67bed9028a00f91
Kevin Simpson <relasine@gmail.com>
2018-12-19 16:21:21 -0700

    Add background to timer

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
-import Login from '../Login/Login';
-import Signup from '../Signup/Singup';
+import React, { Component } from "react";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
 
 class Landing extends Component {
-	constructor() {
-		super();
-		this.state = {
-			login: true,
-			signup: false
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      login: true,
+      signup: false
+    };
+  }
 
-	landingChange = () => {
-		this.setState({login: !this.state.login, signup: !this.state.signup})
-	}
+  landingChange = () => {
+    this.setState({ login: !this.state.login, signup: !this.state.signup });
+  };
 
-	render() {
-		return (
-			<main>
-				<header>
-					<h1>Pitch Battles</h1>
-					<div className='logo' />
-				</header>
-				<section className='login-signup-container'>
-					<Login landingChange={this.landingChange} status={this.state.login} />
-					<Signup landingChange={this.landingChange} status={this.state.signup} />
-				</section>
-			</main>
-		)
-	}
+  render() {
+    return (
+      <main>
+        <header>
+          <h1>Pitch Battles</h1>
+          <div className="logo" />
+        </header>
+        <section className="login-signup-container">
+          <Login landingChange={this.landingChange} status={this.state.login} />
+          <Signup
+            landingChange={this.landingChange}
+            status={this.state.signup}
+          />
+        </section>
+      </main>
+    );
+  }
 }
 
 export default Landing;
