6e1d076bd19b2b3ac3b22da55d002867427ef593
Kevin Simpson <relasine@gmail.com>
2018-12-19 16:56:49 -0700

    Finish layout and positioning for login/signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
-import './Login.css';
+import React, { Component } from "react";
+import "./Login.css";
 
 class Login extends Component {
-	constructor() {
-		super();
-		this.state = {
-			email: '',
-			password: ''
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-	handleChange = (e) => {
-		this.setState({[e.target.name]: e.target.value})
-	}
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-	render() {
-		return (
-			<section className={`login-form ${this.props.status}`} >
-				<form>
-				  <div className='email-input'>
-					<label>email</label>
-					<input type='email' name='email' onChange={this.handleChange} value={this.state.email} />
-				  </div>
-				  <div className='password-input'>
-					<label>password</label>
-					<input type='password' name='password' onChange={this.handleChange} value={this.state.password} />
-				  </div>
-					<button>Login</button>
-				</form>
-				<button onClick={this.props.landingChange}>Sign Up</button>
-			</section>
-		)
-	}
+  render() {
+    return (
+      <section className={`login-form ${this.props.status}`}>
+        <form>
+          <input
+            placeholder="email"
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+          />
+          <input
+            placeholder="password"
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+          <button>Login</button>
+        </form>
+        <button onClick={this.props.landingChange}>Sign Up</button>
+      </section>
+    );
+  }
 }
 
 export default Login;
