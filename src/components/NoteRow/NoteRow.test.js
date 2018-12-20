d902fd02d6407ee2ac117732f3cef495debc31e8
Kevin Simpson <relasine@gmail.com>
2018-12-20 08:58:47 -0700

    Add three missing test file setups

---
+++ src/components/NoteRow/NoteRow.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NoteRow from "./NoteRow";
+
+it("is a test", () => {});
---
+++ src/components/Timer/Timer.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Timer from "./Timer";
+
+it("is a test", () => {});
