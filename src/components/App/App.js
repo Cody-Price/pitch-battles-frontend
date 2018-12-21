89e93778a86b860d7bfc27fa6cd781791059faab
Kevin Simpson <relasine@gmail.com>
2018-12-20 18:03:43 -0700

    Comment out unfinished code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import Game from "../Game/Game";
-import Landing from "../Landing/Landing";
+// import Landing from "../Landing/Landing";
 
 import "./App.css";
 
@@ -13,22 +13,22 @@ class App extends Component {
     };
   }
 
-  signUpUser = async body => {
-    console.log(body);
-    const url = "https://pitch-battles-api.herokuapp.com/api/v1/users";
-    const options = {
-      body: JSON.stringify(body),
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json; charset-uts-8"
-      }
-    };
-    const response = await fetch(url, options);
+  // signUpUser = async body => {
+  //   console.log(body);
+  //   const url = "https://pitch-battles-api.herokuapp.com/api/v1/users";
+  //   const options = {
+  //     body: JSON.stringify(body),
+  //     method: "POST",
+  //     mode: "cors",
+  //     headers: {
+  //       "Content-Type": "application/json; charset-uts-8"
+  //     }
+  //   };
+  //   const response = await fetch(url, options);
 
-    const parseReponse = await response.json();
-    // console.log(parseReponse)
-  };
+  //   const parseReponse = await response.json();
+  //   // console.log(parseReponse)
+  // };
 
   render() {
     return (
