c329a8a4940bd8e51a43a94abfde55ab2bccb197
Kevin Simpson <relasine@gmail.com>
2018-12-21 08:40:03 -0700

    Add characters nine and ten

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -8,6 +8,9 @@ import "./CharacterAnims/Char4.css";
 import "./CharacterAnims/Char5.css";
 import "./CharacterAnims/Char6.css";
 import "./CharacterAnims/Char7.css";
+import "./CharacterAnims/Char8.css";
+import "./CharacterAnims/Char9.css";
+import "./CharacterAnims/Char10.css";
 
 class Player extends Component {
   render() {
