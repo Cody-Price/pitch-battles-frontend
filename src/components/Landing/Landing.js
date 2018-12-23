fd15372b4dc23a87945ab3d061c64780876f1cb5
Kevin Simpson <relasine@gmail.com>
2018-12-22 22:54:36 -0700

    Add third background and floaty crystals

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -26,6 +26,18 @@ class Landing extends Component {
           <div className="landing-birb" />
           <div className="standing-char1" />
           <div className="standing-char2" />
+          <div className="crystal1" />
+          <div className="crystal2" />
+          <div className="crystal3" />
+          <div className="crystal4" />
+          <div className="crystal5" />
+          <div className="crystal6" />
+          <div className="crystal7" />
+          <div className="crystal8" />
+          <div className="crystal9" />
+          <div className="crystal10" />
+          <div className="crystal11" />
+          <div className="crystal12" />
         </div>
 
         <header className="landing-page-header">
