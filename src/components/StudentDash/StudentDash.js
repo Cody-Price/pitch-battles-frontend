57afb00dbe1365b29d76352406909ac436477fac
Kevin Simpson <relasine@gmail.com>
2018-12-23 13:21:41 -0700

    Layout studentDash

--- src/components/StudentDash/StudentDash.js
+++ src/components/StudentDash/StudentDash.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./StudentDash.css";
 
+import avatars from "../../utilities/avatars";
 import { instruments } from "../../utilities/instruments";
 
 class StudentDash extends Component {
@@ -21,7 +22,8 @@ class StudentDash extends Component {
 
   selectInstrument = instrument => {
     this.setState({
-      instrument
+      instrument,
+      dropdownDeploy: false
     });
   };
 
@@ -33,6 +35,7 @@ class StudentDash extends Component {
             this.selectInstrument(instrument.instrument);
           }}
           className="instrument-list-item"
+          key={instrument.instrument}
         >
           {instrument.instrument}
         </li>
@@ -41,40 +44,69 @@ class StudentDash extends Component {
 
     return (
       <main className="student-dash">
-        <header className="student-dash-header">
-          <div className="student-dash-avatar-backer">
-            <div className="student-dash-avatar" />
-          </div>
-          <h2 className="student-name">
-            name: <span>{this.props.user.first_name}</span>
-          </h2>
-          <h2 className="student-class-link">
-            class: <span>{this.props.user.class}</span>
-          </h2>
-          <div className="onboarding-link" />
-        </header>
-        <section className="new-game-section">
-          <button className="start-game">to battle!</button>
-          <h3
-            onClick={this.handleInstrumentDropdown}
-            className="instrument-dropdown"
-          >
-            {this.state.instrument}
-          </h3>
-          <ul
-            className={`instrument-dropdown-list ${this.state.dropdownDeploy}`}
-          >
-            {instrumentList}
-          </ul>
-        </section>
-        <section className="student-dash-stats">
-          <h2 className="total-games-played">
-            total battles fought: <span />
-          </h2>
-          <h2 className="most-recent-badges">most recent badges:</h2>
-          <div className="badge-row">
-            <div />
-          </div>
+        <div className="student-dash-floating-backer">
+          <div className="landing-char1" />
+          <div className="landing-char2" />
+          <div className="landing-birb" />
+          <div className="standing-char1" />
+          <div className="standing-char2" />
+          <div className="crystal1" />
+          <div className="crystal2" />
+          <div className="crystal3" />
+          <div className="crystal4" />
+          <div className="crystal5" />
+          <div className="crystal6" />
+          <div className="crystal7" />
+          <div className="crystal8" />
+          <div className="crystal9" />
+          <div className="crystal10" />
+          <div className="crystal11" />
+          <div className="crystal12" />
+        </div>
+        <section className="student-dash-wrapper">
+          <header className="student-dash-header">
+            <div className="student-dash-avatar-backer">
+              <div
+                className={`student-dash-avatar ${
+                  avatars[this.props.user.avatar]
+                }`}
+              />
+            </div>
+            <section className="header-data">
+              <h2 className="student-name">
+                {this.props.user.first_name} {this.props.user.last_name}
+              </h2>
+              <h2 className="student-class-link">
+                class: <span>{this.props.user.class}</span>
+              </h2>
+            </section>
+            <button className="onboarding-link" />
+          </header>
+          <section className="new-game-section">
+            <button className="start-game">to battle!</button>
+            <h3
+              onClick={this.handleInstrumentDropdown}
+              className="instrument-dropdown"
+            >
+              {this.state.instrument}
+            </h3>
+            <ul
+              className={`instrument-dropdown-list ${
+                this.state.dropdownDeploy
+              }`}
+            >
+              {instrumentList}
+            </ul>
+          </section>
+          <section className="student-dash-stats">
+            <h2 className="total-games-played">
+              total battles fought: <span>{this.props.user.games_played}</span>
+            </h2>
+            <h2 className="most-recent-badges">most recent badges:</h2>
+            <div className="badge-row">
+              <div />
+            </div>
+          </section>
         </section>
       </main>
     );
---
+++ src/utilities/mockUser.js
@@ -0,0 +1,11 @@
+const mockUser = {
+  first_name: "Chet",
+  last_name: "Manly",
+  avatar: 10,
+  role: 0,
+  email: "cmanly@gmail.com",
+  games_played: 100,
+  class: "6th-grade band"
+};
+
+export default mockUser;
