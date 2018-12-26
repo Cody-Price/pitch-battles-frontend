c5375cf53bb20b8c4b454bebb2ce08667a6a3182
Kevin Simpson <relasine@gmail.com>
2018-12-25 20:18:54 -0700

    Write and test EditProfile

---
+++ src/components/EditProfile/EditProfile.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import "./EditProfile.css";
+
+class EditProfile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: ""
+    };
+  }
+
+  componentDidMount() {
+    this.populateOnLoad();
+  }
+
+  populateOnLoad = () => {
+    this.setState({
+      first_name: this.props.user.first_name,
+      last_name: this.props.user.last_name,
+      email: this.props.user.email
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    if (
+      this.state ===
+      {
+        first_name: "",
+        last_name: "",
+        email: ""
+      }
+    ) {
+      return;
+    }
+    const update = {
+      first_name: this.state.first_name || this.props.user.first_name,
+      last_name: this.state.last_name || this.props.user.last_name,
+      email: this.state.email || this.props.user.email
+    };
+
+    this.props.changeProfile(update);
+  };
+
+  render() {
+    return (
+      <section className="edit-profile">
+        <form className="edit-profile-form" onSubmit={this.handleSubmit}>
+          <input
+            name="first_name"
+            className="profile-first-name-input"
+            value={this.state.first_name}
+            placeholder="first name"
+            onChange={event => {
+              this.handleChange(event);
+            }}
+          />
+          <input
+            name="last_name"
+            className="profile-last-name-input"
+            value={this.state.last_name}
+            placeholder="last name"
+            onChange={event => {
+              this.handleChange(event);
+            }}
+          />
+          <input
+            type="email"
+            name="email"
+            className="profile-email-input"
+            value={this.state.email}
+            placeholder="email"
+            onChange={event => {
+              this.handleChange(event);
+            }}
+          />
+          <button type="submit" className="profile-submit-btn">
+            submit new info
+          </button>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default EditProfile;
