85790f12a521c71df774dcf61c5039d89067448d
Kevin Simpson <relasine@gmail.com>
2018-12-27 13:02:22 -0700

    Add LoadingAnimation component and update snapshot

---
+++ src/LoadingAnimation/LoadingAnimation.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./LoadingAnimation.css";
+
+class LoadingAnimation extends Component {
+  render() {
+    return <div className="loading-animation" />;
+  }
+}
+
+export default LoadingAnimation;
