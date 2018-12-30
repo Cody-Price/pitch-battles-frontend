e5604f5a1a1b1bc6461acbb7ba4cc323abe27995
Kevin Simpson <relasine@gmail.com>
2018-12-29 20:37:35 -0700

    Refactor and add fetch calls for post-game analysis and profile edits

--- src/utilities/fetchCalls.js
+++ src/utilities/fetchCalls.js
@@ -42,30 +42,93 @@ export const userFetch = async webToken => {
     }
   };
 
-  console.log(options);
-
   const response = await fetch(url, options);
   return await response.json();
 };
 
 export const postGameUserUpdate = async (update, webToken) => {
-  const url = "https://pitch-battles-api.herokuapp.com/api/v1/users";
-
-  const body = update;
+  console.log(update, webToken);
+  const url = "https://pitch-battles-api.herokuapp.com/api/v1/games/";
 
   const options = {
-    body: JSON.stringify(body),
+    body: JSON.stringify(update),
     method: "POST",
     mode: "cors",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      Authorization: `bearer ${webToken}`
     }
   };
-  return;
+
+  const response = await fetch(url, options);
+  return await response.json();
 };
 
 export const forgotMyPasswordCall = async email => {
   // const url
   // return response
   return Promise.resolve(true);
+};
+
+export const changeAvatarFetch = async (avatar, id, webToken) => {
+  const url = `https://pitch-battles-api.herokuapp.com/api/v1/users/${id}`;
+
+  const body = { avatar: avatar };
+
+  const options = {
+    body: JSON.stringify(body),
+    method: "PATCH",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${webToken}`
+    }
+  };
+
+  const response = await fetch(url, options);
+  return await response.json();
+};
+
+export const changeProfileFetch = async (name, id, webToken) => {
+  const url = `https://pitch-battles-api.herokuapp.com/api/v1/users/${id}`;
+
+  const options = {
+    body: JSON.stringify(name),
+    method: "PATCH",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${webToken}`
+    }
+  };
+
+  const response = await fetch(url, options);
+  return await response.json();
+};
+
+export const changePassword = async (
+  oldPassword,
+  newPassword,
+  id,
+  webToken
+) => {
+  const url = `https://pitch-battles-api.herokuapp.com/api/v1/users/${id}`;
+
+  const body = {
+    current_password: oldPassword,
+    password: newPassword
+  };
+
+  const options = {
+    body: JSON.stringify(body),
+    method: "PATCH",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${webToken}`
+    }
+  };
+
+  const response = await fetch(url, options);
+  return await response.json();
 };
