7af1a21cba2d7a376566c576398133afd9a3521d
Kevin Simpson <relasine@gmail.com>
2018-12-31 15:40:25 -0700

    Add exiting/resetting messages to game modal

--- src/components/GameUserModal/GameUserModal.js
+++ src/components/GameUserModal/GameUserModal.js
@@ -8,7 +8,10 @@ class GameUserModal extends Component {
         <h4 className="game-modal-username">{this.props.username}</h4>
         <p className="current-instrument-label">instrument</p>
         <p className="current-instrument">{this.props.instrument}</p>
-
+        <p className={`${this.props.resetting} resetting-message`}>
+          resetting...
+        </p>
+        <p className={`${this.props.exiting} exiting-message`}>exiting...</p>
         <button onClick={this.props.reset} className="reset-game-button">
           reset
         </button>
