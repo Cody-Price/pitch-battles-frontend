4af13d02ffbdf59264651716e94facd668b4d938
Kevin Simpson <relasine@gmail.com>
2018-12-31 15:39:52 -0700

    Add click-to-exit on GameOver component

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -4,8 +4,9 @@ import "./GameOver.css";
 class GameOver extends Component {
   render() {
     return (
-      <section className="game-over">
+      <section className="game-over" onClick={this.props.exit}>
         <h4 className="game-over-text">GAME OVER</h4>
+        <p className="exit-message">click here to exit...</p>
       </section>
     );
   }
