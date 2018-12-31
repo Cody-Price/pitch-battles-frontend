da214116585060a660cd36ec1433b88ac60fa858
Kevin Simpson <relasine@gmail.com>
2018-12-31 16:27:32 -0700

    Add prop-types

--- src/components/LoadingAnimation/LoadingAnimation.js
+++ src/components/LoadingAnimation/LoadingAnimation.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import "./LoadingAnimation.css";
 
 class LoadingAnimation extends Component {
@@ -23,3 +25,7 @@ class LoadingAnimation extends Component {
 }
 
 export default LoadingAnimation;
+
+LoadingAnimation.propTypes = {
+  animals: PropTypes.string
+};
