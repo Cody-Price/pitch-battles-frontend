32e54491e0903130a78c7d577f8604f1d867e486
Kevin Simpson <relasine@gmail.com>
2019-01-01 20:41:14 -0700

    Refactor landing for password reset

---
+++ src/components/ResetPassword/ResetPassword.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import "./ResetPassword.css";
+
+import { resetPasswordFetch } from "../../utilities/fetchCalls";
+
+class ResetPassword extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      password: "",
+      confirmPassword: "",
+      webToken: "",
+      passwordMatchError: false,
+      webTokenError: false,
+      success: false
+    };
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this._timeout);
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    if (this.state.password !== this.state.confirmPassword) {
+      this.setState({
+        passwordMatchError: true,
+        webTokenError: false,
+        success: false
+      });
+      this._timeout = setTimeout(this.clearErrors, 5000);
+      return;
+    }
+
+    try {
+      const data = await resetPasswordFetch(
+        this.state.password,
+        this.state.webToken
+      );
+      if (data.error) {
+        throw data.error;
+      } else {
+        this.setState({
+          webTokenError: false,
+          passwordMatchError: false,
+          success: true
+        });
+      }
+      console.log(data);
+    } catch (error) {
+      this.setState({
+        passwordMatchError: false,
+        webTokenError: true,
+        success: false
+      });
+      console.log(error);
+      this._timeout = setTimeout(this.clearErrors, 5000);
+    }
+  };
+
+  clearError = () => {
+    this.setState({
+      passwordMatchError: false,
+      webTokenError: false
+    });
+  };
+
+  render() {
+    return (
+      <article
+        className={`reset-password-container-frame ${this.props.status}`}
+      >
+        <form
+          onSubmit={e => {
+            this.handleSubmit(e);
+          }}
+          className={`reset-password-form `}
+        >
+          <p className="reset-pw-label">reset password</p>
+          <div className="password-reset-icon-one" />
+          <input
+            name="password"
+            value={this.state.password}
+            className="reset-pw-input"
+            placeholder="password"
+            onChange={e => {
+              this.handleChange(e);
+            }}
+          />
+          <div className="password-reset-icon-two" />
+          <input
+            name="confirmPassword"
+            value={this.state.confirmPassword}
+            className="reset-pw-input"
+            placeholder="confirm password"
+            onChange={e => {
+              this.handleChange(e);
+            }}
+          />
+          <div className="webtoken-icon" />
+          <input
+            name="webToken"
+            value={this.state.webToken}
+            className="reset-pw-input"
+            placeholder="reset token"
+            onChange={e => {
+              this.handleChange(e);
+            }}
+          />
+          <button className="reset-pw-button">reset password</button>
+        </form>
+        <p className={`success-message-reset ${this.state.success}`}>
+          password succesfully changed!
+        </p>
+        <p className={`fail-message-reset ${this.state.webTokenError}`}>
+          attempt failed - check your webtoken!
+        </p>
+        <p className={`no-pw-match-reset ${this.state.passwordMatchError}`}>
+          passwords do not match!
+        </p>
+
+        <button onClick={this.props.toLogin} className="back-to-login-btn">
+          login
+        </button>
+      </article>
+    );
+  }
+}
+
+export default ResetPassword;
