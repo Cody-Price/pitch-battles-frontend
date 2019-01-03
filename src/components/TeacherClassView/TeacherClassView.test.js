d07626ffa00bafc1bf983800e5ae2211a72047a1
Kevin Simpson <relasine@gmail.com>
2019-01-02 17:48:05 -0700

    Setup class rendering and api call

---
+++ src/components/TeacherClassView/TeacherClassView.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TeacherClassView from "./TeacherClassView";
+import Enzyme, { shallow } from "enzyme";
+const EnzymeAdapter = require("enzyme-adapter-react-16");
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe("TeacherClassView", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<TeacherClassView />);
+  });
+
+  it("should match the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
