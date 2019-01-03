769d184d3eff5b4e4dd06689059f6a6a4cd3a2e0
Kevin Simpson <relasine@gmail.com>
2019-01-02 17:30:23 -0700

    Style class cards

--- src/components/ClassCard/ClassCard.js
+++ src/components/ClassCard/ClassCard.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import './ClassCard.css';
+import React, { Component } from "react";
+import "./ClassCard.css";
 
 class ClassCard extends Component {
-	constructor() {
-		super();
-		this.state = {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-		}
-	}
-
-	render() {
-		return (
-			<article className='class-card'>
-				<header className='class-card-header'>
-					<h3 className='class-card-name'>{this.props.data.name}</h3>
-				</header>
-				<p className='class-card-key'>{this.props.data.key}</p>
-				<button className='class-select-button' onClick={() => this.props.classSelect(this.props.data.id)}>select</button>
-			</article>
-		)
-	}
+  render() {
+    return (
+      <article className="class-card">
+        <header className="class-card-header">
+          <h3 className="class-card-name">{this.props.data.attributes.name}</h3>
+        </header>
+        <p className="class-card-key">
+          key: {this.props.data.attributes.class_key}
+        </p>
+        <button
+          className="class-select-button"
+          onClick={() => this.props.classSelect(this.props.data.id)}
+        >
+          select
+        </button>
+      </article>
+    );
+  }
 }
 
 export default ClassCard;
