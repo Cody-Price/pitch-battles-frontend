d1b4f2a8e60c049e97339329f64a39b04accf6d1
Kevin Simpson <relasine@gmail.com>
2019-01-02 22:47:05 -0700

    Setup and render GameRow component

---
+++ src/components/GameRow/GameRow.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./GameRow.css";
+
+import "../../utilities/gameHelper";
+import { gameHelper } from "../../utilities/gameHelper";
+
+class GameRow extends Component {
+  render() {
+    const { game } = this.props;
+    return (
+      <article className="game-row">
+        <p className="game-id-code">{game.id}</p>
+        <p className="game-score">
+          {game.attributes.level_one_duration || "none"}
+        </p>
+        <div
+          className={`game-perfect-mark ${game.attributes.level_one_perfect}`}
+        />
+        <p className="game-score">
+          {game.attributes.level_two_duration || "none"}
+        </p>
+        <div
+          className={`game-perfect-mark ${game.attributes.level_two_perfect}`}
+        />
+        <p className="game-score">
+          {game.attributes.level_three_duration || "none"}
+        </p>
+        <div
+          className={`game-perfect-mark ${game.attributes.level_three_perfect}`}
+        />
+        <p className="game-score">
+          {game.attributes.level_four_duration || "none"}
+        </p>
+        <div
+          className={`game-perfect-mark ${game.attributes.level_four_perfect}`}
+        />
+      </article>
+    );
+  }
+}
+
+export default GameRow;
