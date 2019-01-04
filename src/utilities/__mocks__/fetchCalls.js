e56f2524351dc8f88766834ba510a7e95fab5498
Kevin Simpson <relasine@gmail.com>
2019-01-03 21:10:28 -0700

    Add jest mocks

--- src/utilities/__mocks__/fetchCalls.js
+++ src/utilities/__mocks__/fetchCalls.js
@@ -1,98 +1,97 @@
 import mockUser from "../../utilities/mockUser";
-import mockStudentClass from "../mockStudentClass";
+import { mockStudentClass } from "../mockStudentClass";
 import { mockTeacherClass } from "../mockTeacherClass";
 
-export const login = async body => {
+export const login = jest.fn(async () => {
   const mockResponse = { access_token: "test" };
   return Promise.resolve(mockResponse);
-};
+});
 
-export const signUp = async body => {
+export const signUp = jest.fn(async () => {
   const mockResponse = { response: {} };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const userFetch = async webToken => {
-  const mockResponse = mockUser;
+export const userFetch = jest.fn(async () => {
+  const mockResponse = { data: mockUser };
   return Promise.resolve(mockResponse);
-};
+});
 
-export const postGameUserUpdate = async (update, webToken) => {
+export const postGameUserUpdate = jest.fn(async (update, webToken) => {
   const mockResponse = { response: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const forgotMyPasswordCall = async email => {
+export const forgotMyPasswordCall = jest.fn(async email => {
   const mockResponse = { response: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const changeAvatarFetch = async (avatar, id, webToken) => {
+export const changeAvatarFetch = jest.fn(async (avatar, id, webToken) => {
   const mockResponse = { reponse: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const changeProfileFetch = async (name, id, webToken) => {
+export const changeProfileFetch = jest.fn(async () => {
   const mockResponse = { response: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const changePasswordFetch = async (
-  oldPassword,
-  newPassword,
-  id,
-  webToken
-) => {
+export const changePasswordFetch = jest.fn(
+  async (oldPassword, newPassword, id, webToken) => {
+    const mockResponse = { response: [] };
+
+    return Promise.resolve(mockResponse);
+  }
+);
+
+export const createClassFetch = jest.fn(async (name, webToken) => {
   const mockResponse = { response: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const createClassFetch = async (name, webToken) => {
+export const joinClassFetch = jest.fn(async (class_key, id, webToken) => {});
+
+export const leaveClassFetch = jest.fn(
+  async (student_id, class_id, webToken) => {
+    const mockResponse = { response: [] };
+
+    return Promise.resolve(mockResponse);
+  }
+);
+
+export const resetPasswordFetch = jest.fn(async (password, token) => {
   const mockResponse = { response: [] };
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const joinClassFetch = async (class_key, id, webToken) => {};
-
-export const leaveClassFetch = async (student_id, class_id, webToken) => {
-  const mockResponse = { response: [] };
-
-  return Promise.resolve(mockResponse);
-};
-
-export const resetPasswordFetch = async (password, token) => {
-  const mockResponse = { response: [] };
-
-  return Promise.resolve(mockResponse);
-};
-
-export const studentClassFetch = async webToken => {
+export const studentClassFetch = jest.fn(async webToken => {
   const mockResponse = mockStudentClass;
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const teacherAllClassesFetch = async webToken => {
+export const teacherAllClassesFetch = jest.fn(async webTokeb => {
   const mockResponse = mockTeacherClass;
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const teacherSpecificClassFetch = async (id, webToken) => {
+export const teacherSpecificClassFetch = jest.fn(async (id, webToken) => {
   const mockResponse = mockTeacherClass;
 
   return Promise.resolve(mockResponse);
-};
+});
 
-export const deleteClassFetch = async (id, webToken) => {
+export const deleteClassFetch = jest.fn(async (id, webToken) => {
   const mockResponse = { data: "success" };
 
   return Promise.resolve(mockResponse);
-};
+});
