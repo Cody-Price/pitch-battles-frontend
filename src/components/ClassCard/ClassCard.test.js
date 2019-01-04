27bd7266210679b903144e5009ac819d173965cf
Kevin Simpson <relasine@gmail.com>
2019-01-03 19:29:51 -0700

    Add basic class card test

--- src/components/ClassCard/ClassCard.test.js
+++ src/components/ClassCard/ClassCard.test.js
@@ -7,6 +7,7 @@ const EnzymeAdapter = require("enzyme-adapter-react-16");
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe("ClassCard", () => {
+  let mockGenerateClassCards;
   const mockData = {
     attributes: {
       name: "5th Grade Band",
@@ -18,9 +19,14 @@ describe("ClassCard", () => {
   let mockClassSelect;
 
   beforeEach(() => {
+    mockGenerateClassCards = jest.fn();
     mockClassSelect = jest.fn();
     wrapper = shallow(
-      <ClassCard data={mockData} classSelect={mockClassSelect} />
+      <ClassCard
+        data={mockData}
+        classSelect={mockClassSelect}
+        generateClassCards={mockGenerateClassCards}
+      />
     );
   });
 
@@ -32,5 +38,71 @@ describe("ClassCard", () => {
     wrapper.find(".class-select-button").simulate("click");
 
     expect(mockClassSelect).toBeCalledWith(1);
+  });
+
+  describe("confirmDelete", () => {
+    it("should call confirmDelete on click", () => {
+      const spy = jest.spyOn(wrapper.instance(), "confirmDelete");
+      wrapper.instance().forceUpdate();
+
+      wrapper.find(".fa-trash-alt").simulate("click");
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it("should call setState", () => {
+      wrapper.instance().confirmDelete();
+
+      expect(wrapper.state().showConfirmDelete).toEqual(true);
+    });
+  });
+
+  describe("deleteClass", () => {
+    it("should call setState", async () => {
+      wrapper.setState({
+        deleteError: true
+      });
+      await wrapper.instance().deleteClass();
+
+      expect(wrapper.state().deleteError).toEqual(false);
+    });
+
+    it("should call generateClassCards", async () => {
+      await wrapper.instance().deleteClass();
+
+      expect(mockGenerateClassCards).toHaveBeenCalled();
+    });
+
+    it("should call deleteClass on click", async () => {
+      const spy = jest.spyOn(wrapper.instance(), "deleteClass");
+
+      wrapper.instance().forceUpdate();
+
+      await wrapper.find(".class-delete-btn").simulate("click");
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe("doNotDelete", () => {
+    it("should setstate", () => {
+      wrapper.setState({
+        showConfirmDelete: true
+      });
+
+      wrapper.instance().doNotDelete();
+
+      expect(wrapper.state().showConfirmDelete).toEqual(false);
+    });
+
+    it("should call doNotDelete on click", () => {
+      const spy = jest.spyOn(wrapper.instance(), "doNotDelete");
+
+      wrapper.instance().forceUpdate();
+
+      wrapper.find(".class-do-not-delete-btn").simulate("click");
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
