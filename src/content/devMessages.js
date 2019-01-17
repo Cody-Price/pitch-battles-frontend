3967af693989054460efd7ec03861fe8333d0c01
Kevin Simpson <relasine@gmail.com>
2019-01-17 09:58:47 -0700

    Add devMessage and version content export files

---
+++ src/content/devMessages.js
@@ -0,0 +1,9 @@
+export const devMessages = {
+  date: "Thursday, January 17th, 2018",
+  title: "Database Reset",
+  text: [
+    "Now that Pitch Battles is beginning to see some traffic, we have had to rollback and migrate our database to accomodate a growing user population. If you previously created an account, you will need to create a new one. This should be the last time this happens pending some unforeseen catastrophe.",
+    "Additionally, we have been adding loads of small improvements to the student-facing user interface including support for iOS browsers, but we are still looking for user feedback. If you find any bugs, please send us an email at teampitchbattles@gmail.com!"
+  ],
+  signature: "Team Pitch Battles"
+};
